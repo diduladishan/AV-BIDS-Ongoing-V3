267bbee33ff88ba4a426aa976e9b4cc28fd15bfb
diduladishan <diduladishan14@gmail.com>
2024-03-16 20:36:43 +0530

    billing section responsiveness update

--- client/src/pages/billing/components/page01.tsx
+++ client/src/pages/billing/components/page01.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from "react";
-import Monthly from "../../../assets/17_billing/month_img.png";
-import Yearly from "../../../assets/17_billing/yearly_img.png";
+import React, { useEffect, useState } from 'react';
+import Monthly from '../../../assets/17_billing/month_img.png';
+import Yearly from '../../../assets/17_billing/yearly_img.png';
 
-import { setPrice } from "../../../app/features/stripe/stripeSlice";
-import { useAppDispatch } from "../../../app/hooks";
-import { StripePrice } from "../../../types";
-import api from "../../../utils/api";
-import PaymentPlanCard from "./payment-plan-card";
+import { setPrice } from '../../../app/features/stripe/stripeSlice';
+import { useAppDispatch } from '../../../app/hooks';
+import { StripePrice } from '../../../types';
+import api from '../../../utils/api';
+import PaymentPlanCard from './payment-plan-card';
 
-import backgroundArt01 from "../../../assets/17_billing/backgroundArt01.png";
-import backgroundArt02 from "../../../assets/17_billing/backgroundArt02.png";
+import backgroundArt01 from '../../../assets/17_billing/backgroundArt01.png';
+import backgroundArt02 from '../../../assets/17_billing/backgroundArt02.png';
 
 interface Page01Props {
   onNext: () => void;
@@ -21,7 +21,7 @@ const Page01: React.FC<Page01Props> = ({ onNext }) => {
 
   useEffect(() => {
     const fetchPrices = async () => {
-      const { data } = await api.get("/stripe/config");
+      const { data } = await api.get('/stripe/config');
       setPrices(data.prices);
       dispatch(setPrice(data.prices));
     };
@@ -30,41 +30,43 @@ const Page01: React.FC<Page01Props> = ({ onNext }) => {
 
   return (
     <div>
-      <p className="text-[#195c87] font-medium text-[28px] mb-2 text-center mt-[100px]">
+      <p className='text-[#195c87] font-medium text-[28px] mb-2 text-center mt-[100px]'>
         AV Bids - Powering Event Connections!
       </p>
-      <p className="text-[#4d5768]  text-[16px] mb-2 text-center">
+      <p className='text-[#4d5768]  text-[16px] mb-2 text-center'>
         Yes, AV Bids offers paid services to help event planners plan and
         prepare their events for success.
       </p>
 
-      <div className="flex justify-center gap-6 mb-20  relative">
-        <div className="absolute top-0 left-[0px] hidden lg:block">
+      <div className='flex justify-center gap-6 mb-20  relative'>
+        <div className='absolute top-0 left-[0px] hidden lg:block'>
           <img
             src={backgroundArt01}
-            alt="background img 01 for billing page"
-            className="w-[230px] hidden 2xl:block"
+            alt='background img 01 for billing page'
+            className='w-[230px] hidden 2xl:block'
           />
         </div>
 
-        <div className="absolute top-0 right-[0px] ">
+        <div className='absolute top-0 right-[0px] '>
           <img
             src={backgroundArt02}
-            alt="background img 02 for billing page"
-            className="w-[230px] hidden 2xl:block"
+            alt='background img 02 for billing page'
+            className='w-[230px] hidden 2xl:block'
           />
         </div>
 
-        {prices.map((price) => (
-          <PaymentPlanCard
-            key={price.id}
-            src={price.recurring.interval === "year" ? Yearly : Monthly}
-            plan={price}
-            price={price.unit_amount / 100}
-            priceId={price.id}
-            onNext={onNext}
-          />
-        ))}
+        <div className='xl:flex gap-6'>
+          {prices.map((price) => (
+            <PaymentPlanCard
+              key={price.id}
+              src={price.recurring.interval === 'year' ? Yearly : Monthly}
+              plan={price}
+              price={price.unit_amount / 100}
+              priceId={price.id}
+              onNext={onNext}
+            />
+          ))}
+        </div>
 
         {/* <div className='text-primary_font_color px-8 pt-4'>
           <p className='font-medium mb-4 text-primary_font_color '>
--- client/src/pages/billing/components/payment-plan-card.tsx
+++ client/src/pages/billing/components/payment-plan-card.tsx
@@ -1,17 +1,17 @@
-import { Spinner } from "@material-tailwind/react";
-import { FC, useState } from "react";
+import { Spinner } from '@material-tailwind/react';
+import { FC, useState } from 'react';
 import {
   setPlan,
   setSubscription,
-} from "../../../app/features/stripe/stripeSlice";
-import { useAppDispatch } from "../../../app/hooks";
-import { useGetCurrentUser } from "../../../app/hooks/useUser";
-import { StripePrice } from "../../../types";
-import api from "../../../utils/api";
+} from '../../../app/features/stripe/stripeSlice';
+import { useAppDispatch } from '../../../app/hooks';
+import { useGetCurrentUser } from '../../../app/hooks/useUser';
+import { StripePrice } from '../../../types';
+import api from '../../../utils/api';
 
 // new imports
-import { MdCheck } from "react-icons/md";
-import { setUser } from "../../../app/features/user/userSlice";
+import { MdCheck } from 'react-icons/md';
+import { setUser } from '../../../app/features/user/userSlice';
 
 interface PaymentPlanCardProps {
   src: string;
@@ -34,12 +34,12 @@ const PaymentPlanCard: FC<PaymentPlanCardProps> = ({
   const [loading, setLoading] = useState(false);
   const [trialLoading, setTrialLoading] = useState(false);
 
-  let key = "defaultKey";
+  let key = 'defaultKey';
 
   const createSubscriptionWithTrial = async () => {
     setTrialLoading(true);
     try {
-      const { data } = await api.post("/stripe/create-subscription-trial", {
+      const { data } = await api.post('/stripe/create-subscription-trial', {
         priceId,
         email: user?.email,
       });
@@ -58,7 +58,7 @@ const PaymentPlanCard: FC<PaymentPlanCardProps> = ({
   const createSubscription = async () => {
     setLoading(true);
     try {
-      const { data } = await api.post("/stripe/create-subscription", {
+      const { data } = await api.post('/stripe/create-subscription', {
         priceId,
         email: user?.email,
       });
@@ -74,60 +74,60 @@ const PaymentPlanCard: FC<PaymentPlanCardProps> = ({
 
   return (
     <div key={key}>
-      <div className="flex items-center justify-center bg-[#fff] border border-[#8645ff] rounded-md w-max px-12 mt-16">
-        <div className="">
-          <div className="flex items-center justify-center">
-            <img src={src} alt="card-pic" className="object-scale-down my-8" />
+      <div className='flex items-center justify-center bg-[#fff] border border-[#8645ff] rounded-md w-max sm:px-12 px-6 mt-16'>
+        <div className=''>
+          <div className='flex items-center justify-center'>
+            <img src={src} alt='card-pic' className='object-scale-down my-8' />
           </div>
 
-          <div className="border-b border-[#C0C0C0]">
-            <p className="mb-1 text-center">
-              <span className="font-semibold text-[18px] text-primary_font_color text-center">
+          <div className='border-b border-[#C0C0C0]'>
+            <p className='mb-1 text-center'>
+              <span className='font-semibold text-[18px] text-primary_font_color text-center'>
                 ${price}
               </span>
-              / {plan.recurring.interval === "year" ? "yearly" : "monthly"}
+              / {plan.recurring.interval === 'year' ? 'yearly' : 'monthly'}
             </p>
 
-            <p className="text-[#828b9b] mb-2 text-center">
+            <p className='text-[#828b9b] mb-2 text-center'>
               {plan.product.name}
             </p>
           </div>
 
-          <div className="grid grid-cols-1 mt-6">
-            <div className="flex gap-2">
-              <MdCheck className="text-[20px] text-[#33da98] " />
-              <p className="text-primary_font_color mb-2">
+          <div className='grid grid-cols-1 mt-6'>
+            <div className='flex gap-2'>
+              <MdCheck className='text-[20px] text-[#33da98] ' />
+              <p className='text-primary_font_color mb-2'>
                 Swift Proposal Submission
               </p>
             </div>
           </div>
 
-          <div className="grid grid-cols-1">
-            <div className="flex gap-2">
-              <MdCheck className="text-[20px] text-[#33da98] " />
-              <p className="text-primary_font_color mb-2">
+          <div className='grid grid-cols-1'>
+            <div className='flex gap-2'>
+              <MdCheck className='text-[20px] text-[#33da98] ' />
+              <p className='text-primary_font_color mb-2'>
                 Effortless Team Collaboration
               </p>
             </div>
           </div>
 
-          <div className="grid grid-cols-1">
-            <div className="flex gap-2">
-              <MdCheck className="text-[20px] text-[#33da98] " />
-              <p className="text-primary_font_color mb-2">
+          <div className='grid grid-cols-1'>
+            <div className='flex gap-2'>
+              <MdCheck className='text-[20px] text-[#33da98] ' />
+              <p className='text-primary_font_color mb-2'>
                 Seamless Event Management Hub
               </p>
             </div>
           </div>
 
           <div
-            className="flex items-center jusitfy-center"
+            className='flex items-center jusitfy-center'
             onClick={() => createSubscriptionWithTrial()}
           >
-            <div className="w-full  border-solid border-2 border-[#8645ff] bg-[#8645ff] hover:bg-[#752dfb] hover:text-white cursor-pointer mt-4 mb-5">
-              <div className="flex items-center justify-center">
-                {trialLoading && <Spinner className="h-4 w-4 mr-2 " />}
-                <p className="text-[#fff] text-[18px] text-center px-4 py-2 hover:text-white">
+            <div className='w-full  border-solid border-2 border-[#8645ff] bg-[#8645ff] hover:bg-[#752dfb] hover:text-white cursor-pointer mt-4 mb-5'>
+              <div className='flex items-center justify-center'>
+                {trialLoading && <Spinner className='h-4 w-4 mr-2 ' />}
+                <p className='text-[#fff] text-[18px] text-center px-4 py-2 hover:text-white'>
                   Start Free Trial
                 </p>
               </div>
@@ -135,13 +135,13 @@ const PaymentPlanCard: FC<PaymentPlanCardProps> = ({
           </div>
 
           <div
-            className="flex items-center jusitfy-center"
+            className='flex items-center jusitfy-center'
             onClick={() => createSubscription()}
           >
-            <div className="w-full  border-solid border-2 border-[#8645ff]  hover:bg-[#752dfb] hover:text-white cursor-pointer mt-4 mb-10">
-              <div className="flex items-center justify-center">
-                {loading && <Spinner className="h-4 w-4 mr-2 " />}
-                <p className="text-[#8645ff] text-[18px] text-center px-4 py-2 hover:text-white">
+            <div className='w-full  border-solid border-2 border-[#8645ff]  hover:bg-[#752dfb] hover:text-white cursor-pointer mt-4 mb-10'>
+              <div className='flex items-center justify-center'>
+                {loading && <Spinner className='h-4 w-4 mr-2 ' />}
+                <p className='text-[#8645ff] text-[18px] text-center px-4 py-2 hover:text-white'>
                   Subscribe Now
                 </p>
               </div>
